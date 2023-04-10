e20203604169b018a97fb4edbf038124fc586a4c
najmus2808 <najmus2808.com>
2023-04-10 11:54:26 +0600

    Solve create a new array with the first letter of each string

---
+++ 4_letterArray.js
@@ -0,0 +1,13 @@
+// Problem-3: Given an array of strings, create the new array with the first letter of each string.
+
+function generateLetterArray(array, position = 0) {
+    let letterArray = [];
+    for (let i = 0; i < array.length; i++) {
+        letterArray.push(array[i][position]);
+    }
+
+    return letterArray;
+}
+
+const strings = ['hello', 'world', 'foo', 'bar'];
+console.log(generateLetterArray(strings));
